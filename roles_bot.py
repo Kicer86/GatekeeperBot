a8b30e1be64049d0996d17db152b8cb9c6de4673
Michał Walenciak <michalwalenciak@gmail.com>
2024-12-09 21:59:57 +0100

    Make bot write to dedicated channel only

--- roles_bot.py
+++ roles_bot.py
@@ -10,15 +10,23 @@ class RolesSource:
 
 
 class RolesBot(discord.Client):
-    def __init__(self, roles_source: RolesSource):
+    def __init__(self, dedicated_channel: str, roles_source: RolesSource):
         intents = discord.Intents.default()
         intents.message_content = True
         intents.members = True
         super().__init__(intents = intents)
         self.roles_source = roles_source
+        self.channel_name = dedicated_channel
+        self.channel = None
 
     async def on_ready(self):
         print(f"Bot is ready as {self.user}")
+
+        if len(self.guilds) != 1:
+            raise RuntimeError(f"Invalid number of guilds: {len(self.guilds)}")
+
+        guild = self.guilds[0]
+        self.channel = discord.utils.get(guild.channels, name=self.channel_name)
 
     async def on_message(self, message):
         if self.user in message.mentions and len(message.mentions) == 1:
@@ -29,10 +37,13 @@ class RolesBot(discord.Client):
             print(message.content)
 
             if message.content == f"<@{self.user.id}> refresh":
-                async with message.channel.typing():
-                    await self._refresh_roles(message.guild.members, message.channel)
+                async with self.channel.typing():
+                    await self._refresh_roles(message.guild.members)
 
-    async def _refresh_roles(self, members, channel):
+    async def _write_to_dedicated_channel(self, message: str):
+        await self.channel.send(message)
+
+    async def _refresh_roles(self, members):
         expected_roles = self.roles_source.get_expected_roles()
 
         no_data_for = []
@@ -74,17 +85,17 @@ class RolesBot(discord.Client):
             for user, roles in added_roles.items():
                 added_roles_status += f"{user}: {", ".join(roles)}\n"
 
-            await channel.send(added_roles_status)
+            await self._write_to_dedicated_channel(added_roles_status)
 
         if len(removed_roles) > 0:
             removed_roles_status = "Role zabrane użytkownikom:\n"
             for user, roles in removed_roles.items():
                 removed_roles_status += f"{user}: {", ".join(roles)}\n"
 
-            await channel.send(removed_roles_status)
+            await self._write_to_dedicated_channel(removed_roles_status)
 
         if len(no_data_for) > 0:
             no_data_for_status = "Użytkownicy obecni na serwerze, dla których brak danych w bazie danych:\n"
             no_data_for_status += ", ".join(no_data_for)
 
-            await channel.send(no_data_for_status)
+            await self._write_to_dedicated_channel(no_data_for_status)
