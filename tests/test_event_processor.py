ab6152a94c37b7b9ef0ffe2399da6a13052ba69b
Michał Walenciak <michalwalenciak@gmail.com>
2025-03-11 15:13:11 +0100

    Test correctness of single action execution

---
+++ tests/test_event_processor.py
@@ -0,0 +1,28 @@
+
+import asyncio
+import unittest
+
+from unittest.mock import MagicMock
+
+from ..event_processor import *
+
+
+class TestRolesBot(unittest.IsolatedAsyncioTestCase):
+    async def test_single_reaction_on_message(self):
+        loop = asyncio.get_running_loop()
+
+        actionMock = EventActions(
+            on_reactionOnMessage = MagicMock(),
+            on_unreactionOnMessage = MagicMock()
+        )
+        processor = EventProcessor(loop = loop, event_actions = actionMock)
+
+        processor.add_event(type = EventType.ReactionOn, user_id = 123, data = ReactionOnMessage(message_id = 256))
+        await asyncio.sleep(2)
+
+        actionMock.on_reactionOnMessage.assert_called_once_with(123, ReactionOnMessage(message_id = 256))
+        actionMock.on_unreactionOnMessage.assert_not_called()
+
+
+if __name__ == "__main__":
+    unittest.main()
