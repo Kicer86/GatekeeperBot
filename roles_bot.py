735da9f81e4b64b63ded4c36dfdf7fd52cb595e1
Michał Walenciak <michalwalenciak@gmail.com>
2025-05-05 19:34:28 +0200

    Print more details

--- roles_bot.py
+++ roles_bot.py
@@ -848,6 +848,7 @@ class RolesBot(discord.Client):
             channel = await guild.fetch_channel(thread_id)
             if channel is None:
                 self.logger.error(f"Could not fetch channel/thread with id {thread_id}")
+                await self._write_to_dedicated_channel(f"Kanał {thread_id} nie istnieje.")
                 continue
 
             self.logger.debug(f"Pinging channel {channel.name}")
@@ -858,6 +859,8 @@ class RolesBot(discord.Client):
                 await self._write_to_dedicated_channel(f"Brak praw by pingować kanał {channel_link} ({channel.name})")
             else:
                 await channel.delete_messages([message])
+
+        await self._write_to_dedicated_channel("Zakończono odświeżanie kanałów")
 
 
     async def _reset_names(self, members: List[discord.Member]):
